c03245fb5470c8af8f50646655c49da9cbe774d3
JxckWilsxn <40690756@live.napier.ac.uk>
2025-04-22 16:17:24 +0100

    Further adjustments during merge

--- music_javascript.js
+++ music_javascript.js
@@ -1,3 +1,25 @@
+const userProgress = document.getElementById('userProgress');
+const progressAmount = document.getElementById('progressAmount');
+const correctAnswer = document.querySelectorAll('.correct-answer');
+
+let userProgressionLevel = 0;
+progressAmount.innerHTML = 0 + '%';
+
+let addUserProgress = () => {
+    if (userProgressionLevel < 100) {
+        userProgressionLevel += 5;
+        userProgress.style.width = userProgressionLevel + '%';
+        progressAmount.innerHTML = userProgressionLevel + '%';
+    } else if (userProgressionLevel == 100) {
+        open("completion.html")
+    }
+}
+
+correctAnswer.forEach(button => {
+    button.addEventListener('click', addUserProgress);
+});
+
+
 
 
 const jazz_quiz_questions = [
